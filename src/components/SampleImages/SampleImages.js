db3546d4a9beaab347e0f80b923d3085eb59eecd
Nasim Reza <nasim8382@gmail.com>
2022-04-04 00:41:08 +0600

    sample images page added

--- src/components/SampleImages/SampleImages.js
+++ src/components/SampleImages/SampleImages.js
@@ -10,10 +10,10 @@ useEffect( () => {
 }, [])
 
     return (
-        <div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 my-16">
             {
-                images.map(image => <div key={image.id} className="images">
-                    <img src={image.img} alt="drone-image" />
+                images.map(image => <div key={image.id}>
+                    <img src={image.img} className="w-72 h-44 md:w-80 md:h-52 lg:w-96 lg:h-60 mx-auto rounded-md" alt="drone-image" />
                 </div>)
             }
         </div>
